e1f776aa07d9235b0cd2f3a387b84c84c78ae608
damminhtien <damminhtienchl@gmail.com>
2017-11-03 18:56:23 +0700

    sua paging

--- public/trangchu.js
+++ public/trangchu.js
@@ -1,12 +1,12 @@
 var arrNguoiDang = [];
 $('document').ready(() => {
-	$(".ketqua").hide();
-	$("#alert").hide();
+    $(".ketqua").hide();
+    $("#alert").hide();
     $("#btnSubmit").click(() => {
-    	arrNguoiDang = [];
-    	$("#result1").val(0);
-    	$("#alert").show();
-    	$("#alert").html("Đợi giây lát");
+        arrNguoiDang = [];
+        $("#result1").val(0);
+        $("#alert").show();
+        $("#alert").html("Đợi giây lát");
         var id = $("#idAnh").val();
         $("#alert").show();
         (() => {
@@ -18,7 +18,7 @@ $('document').ready(() => {
                     $("#result1").val(0);
                     $("#nguoidang").val("");
                     getAjax("https://graph.facebook.com/v2.9/" + id + "/sharedposts?limit=1000?summary=true&access_token=" + acc);
-                    $("#img").attr('src',"https://graph.facebook.com/v2.9/" + id + "/picture?access_token=" + acc);
+                    $("#img").attr('src', "https://graph.facebook.com/v2.9/" + id + "/picture?access_token=" + acc);
                 }
             });
         })();
@@ -34,20 +34,22 @@ function getAjax(url) {
             success: function(data) {
                 var l = data.data.length;
                 for (var i = 0; i < l; i++) {
-                    idNguoiDang = data.data[i].id.substring(0,15);
-                    if(arrNguoiDang.indexOf(idNguoiDang) == -1){
-                    	arrNguoiDang.push(idNguoiDang);
-                    	$("#nguoidang").val( $("#nguoidang").val()+ "Chấp nhận: " + data.data[i].story + "\n");
-                    }else{
-                    	$("#nguoidang").val( $("#nguoidang").val()+"Loại bỏ__: " + data.data[i].story + "\n");
-                    } 
+                    idNguoiDang = data.data[i].id.substring(0, 15);
+                    if (arrNguoiDang.indexOf(idNguoiDang) == -1) {
+                        arrNguoiDang.push(idNguoiDang);
+                        $("#nguoidang").val($("#nguoidang").val() + "Chấp nhận: " + data.data[i].story + "\n");
+                    } else {
+                        $("#nguoidang").val($("#nguoidang").val() + "Loại bỏ__: " + data.data[i].story + "\n");
+                    }
                 }
                 $("#result1").val(parseInt(arrNguoiDang.length));
-                if (data.paging.next != null) setTimeout((n) => { getAjax(data.paging.next) }, 1000);
-                else {
-                	$("#alert").html("Hoàn thành!!!");
-                	setTimeout(()=>{$("#alert").html("Mọi thắc mắc liên hệ với mình nhé fb.com/damminhtienmain")},5000);
-                }
+                if (data.paging == null) {
+                    $("#alert").html("Hoàn thành!!!");
+                    setTimeout(() => { $("#alert").html("Mọi thắc mắc liên hệ với mình nhé fb.com/damminhtienmain") }, 5000);
+                } else if (data.paging.next == null) {
+                    $("#alert").html("Hoàn thành!!!");
+                    setTimeout(() => { $("#alert").html("Mọi thắc mắc liên hệ với mình nhé fb.com/damminhtienmain") }, 5000);
+                } else setTimeout((n) => { getAjax(data.paging.next) }, 1000);
                 $("#result").val((parseInt($("#result").val()) + data.data.length));
             }
         });
